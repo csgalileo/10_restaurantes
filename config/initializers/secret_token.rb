7b3bdf244750cd69f48a4dfe281b1ad2fdbf0793
Elguayo <dracozero7@gmail.com>
2011-03-12 21:25:48 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qhdc::Application.config.secret_token = '5c9009bbc01d7ebd0aef16cc02025401b341c556a9be33e3f41710a007df1099ff7a01ec5e745834153a0a7d7b7b4369412fee145d42ca33ddd6b89e72670e04'
