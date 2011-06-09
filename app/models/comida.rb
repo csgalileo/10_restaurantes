58bd2e64dd019bdbde3ccee6c81e536eab2eb532
Elguayo <dracozero7@gmail.com>
2011-06-09 16:48:35 -0600

    probando problema paperclip

--- app/models/comida.rb
+++ app/models/comida.rb
@@ -7,7 +7,7 @@ class Comida < ActiveRecord::Base
 
 has_attached_file :photo, :url => "/images/comidas/:id/:basename.:extension", :path => ":rails_root/public/images/comidas/:id/:basename.:extension"
           
-validates_attachment_presence :photo
+#validates_attachment_presence :photo
 validates_attachment_size :photo, :less_than => 5.megabytes
 validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
