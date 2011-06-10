e49a1dfc2f8b5cc2d39a89feb32a19311032e070
Elguayo <dracozero7@gmail.com>
2011-06-09 19:17:30 -0600

    Se corrige un archivo

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,13 +1,3 @@
 class MenuController < ApplicationController
 
-  def index
-    @ru = RestauranteUser.find_by_user_id(current_user.id)
-
-   respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @ru }
-    end
-
-  end
-
 end
